6b0990ce9c49d7d69ef98a0e5d66f45ac5f717d0
Victor Jordan <victorjordan@fitec.org.br>
2021-07-08 13:35:59 -0300

    new package

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Text } from 'react-native';
 import * as S from './styles';
 
 export function Dashboard() {
   return <S.Container>
-    <S.Title>Dash</S.Title>
+    <S.Header>
+      <S.UserWrapper>
+        
+        <S.UserInfo>
+          <S.Photo source={{uri: 'https://avatars.githubusercontent.com/u/9259187?v=4'}}></S.Photo>
+          <S.User>
+            <S.UserGreeting>Olá,</S.UserGreeting>
+            <S.UserName>Victor</S.UserName>
+          </S.User>
+        </S.UserInfo>
+
+      </S.UserWrapper>
+    </S.Header>
   </S.Container>
 }
 
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,47 @@
 import styled from 'styled-components/native';
+import {RFPercentage, RFValue} from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   background-color: ${({theme}) => theme.colors.background };
   flex: 1;
-  justify-content: center;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)};
+  background-color: ${({theme}) => theme.colors.primary};
+`;
+
+export const UserWrapper = styled.View `
+  padding: 0 24px;
+  width: 100%;
+`
+export const UserInfo = styled.View`
+  flex-direction: row;
   align-items: center;
 `;
 
-export const Title = styled.Text`
-  color: ${({theme}) => theme.colors.title };
-  font-family: ${({theme}) => theme.fonts.bold };
+export const Photo = styled.Image`
+  height: ${RFValue(48)}px;
+  width: ${RFValue(48)}px;
+
+  border-radius: 10px;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+
+export const UserGreeting = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.regular};
 `
+export const UserName = styled.Text`
+  color: ${({theme}) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({theme}) => theme.fonts.bold};
+`;
+
+
